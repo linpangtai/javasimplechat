ecf42c5dd05218327d4b3fa4f2dae350efc1aec7
linpangtai <1401670585@qq.com>
2016-11-13 11:23:51 +0800

    already set up a window of client

--- src/com/wechat/client/ClientWindow.java
+++ src/com/wechat/client/ClientWindow.java
@@ -13,50 +13,77 @@ public class ClientWindow extends JFrame
 {
 	private JPanel WestPanel;
 	private JPanel EastPanel;
+	private JPanel messagePanel;
+	private JPanel buttonPanel;
 	private int height;
 	private int width;
 	private JTextArea viewArea;
 	private JTextField viewField;
 	private JList userList;
+	private JButton sendmessButton;
+	
+	
 	
 	private String[] str = {"monday","Tuesday","wednesday",
 			"Thursday","friday","no",
-			"hello","world","new","yes"};
+			"hello","world","new","yes","ok","but","i","am","your",
+			"father","haha","what","because",
+			"thankyou","wa","we","do","this","together","thankyou",
+			"wa","we","do","this","together"};
 	public ClientWindow()
 	{
-		this.setLayout(new GridLayout(1,2));
-		WestPanel = new JPanel(new BorderLayout());
-		EastPanel = new JPanel();
-	
-		WestPanel.setBackground(Color.white);
-		
-		userList  = new JList(str);	
-		userList.setFixedCellWidth(200);
-		
-		viewArea = new JTextArea();
-		viewField = new JTextField(2);
-		JScrollPane sp1 = new JScrollPane(userList);
-		JScrollPane sp2 = new JScrollPane(viewArea);
-		
-		//MessPane  = new JTextPane();
-		//sendMessPane = new JTextArea(5,40);
-		
-		
-		WestPanel.add(sp2,BorderLayout.CENTER);
-		WestPanel.add(viewField,BorderLayout.SOUTH);
-		
-		
-		EastPanel.add(userList);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		height = (int) screenSize.getHeight();
 		width = (int) screenSize.getWidth();
 		
-		this.add(WestPanel,BorderLayout.WEST);
+		
+		WestPanel = new JPanel(new BorderLayout());
+		EastPanel = new JPanel();
+		EastPanel.setBackground(Color.white);
+		messagePanel = new JPanel(new BorderLayout());
+		
+		WestPanel.setPreferredSize(new Dimension(700,600));
+		EastPanel.setPreferredSize(new Dimension(200,600));
+		
+		
+		WestPanel.setBackground(Color.white);
+		
+		
+		sendmessButton = new JButton("·¢ËÍ");
+		
+		userList  = new JList(str);	
+		JScrollPane sp1 = new JScrollPane(userList);
+		sp1.setPreferredSize(new Dimension(200,600));
+		userList.setFixedCellWidth(200);
+		
+		viewArea = new JTextArea();
+		viewField = new JTextField();
+
+		//sp1.setVerticalScrollBarPolicy( 
+				//JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		
+		JScrollPane sp2 = new JScrollPane(viewArea);
+		
+		//MessPane  = new JTextPane();
+		//sendMessPane = new JTextArea(5,40);
+		messagePanel.add(sp2,BorderLayout.CENTER);
+		messagePanel.add(viewField,BorderLayout.SOUTH);
+		
+		
+		WestPanel.add(messagePanel,BorderLayout.CENTER);
+		WestPanel.add(sendmessButton,BorderLayout.SOUTH);
+		
+		EastPanel.add(sp1);
+		
+		
+		this.add(WestPanel);
 		this.add(EastPanel,BorderLayout.EAST);
 		this.setLocation(width/2-450, height/2-300);
 		this.setSize(900,600);
+		this.setResizable(false);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(3);
+		
 		
 	}
 	public static void main(String args[])
